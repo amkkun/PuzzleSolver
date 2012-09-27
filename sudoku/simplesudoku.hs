91d60f22e6560f41d66046eda5fc1026cf2fb981
amkkun <amkkun@gmail.com>
2012-09-27 17:44:45 +0900

    intersperse -> unwords

--- sudoku/simplesudoku.hs
+++ sudoku/simplesudoku.hs
@@ -6,38 +6,41 @@ import Control.Monad
 
 type Matrix a = [[a]]
 
+main :: IO ()
+main = do
+  sudoku <- forM [1..9] (\_ -> map read . words <$> getLine) 
+  let answers = solve sudoku
+  mapM_ (mapM_ (putStrLn . unwords . map show)) answers
+
 solve :: Matrix Int -> [Matrix Int]
 solve matrix 
   | isFailed matrix = []
-  | isCompleted matrix = [matrix]
+  | isCompleted matrix = return matrix
   | otherwise = update matrix >>= solve
                 
 isFailed :: Matrix Int -> Bool                
-isFailed matrix = any (any duplicate) $ map (map (filter (/= 0))) 
-                  [matrix, transpose matrix, boxs matrix]
-                  
-boxs :: Matrix a -> Matrix a
-boxs = map concat . concat . map (divide 3) . transpose . map (divide 3)
-
-divide :: Int -> [a] -> [[a]]
-divide _ [] = []
-divide n xs = let (first, rest) = splitAt n xs in first : divide n rest    
-
-duplicate :: Eq a => [a] -> Bool
-duplicate [] = False
-duplicate (x:xs) = elem x xs || duplicate xs
+isFailed matrix = anyDup [matrix, transpose matrix, box matrix]
+  where
+    anyDup = any (any (duplicate . filter (/= 0)))
+    box = map concat . concat . map (divide 3) . transpose . map (divide 3)
   
 isCompleted :: Matrix Int -> Bool
 isCompleted = all (all (/= 0))
   
 update :: Matrix Int -> [Matrix Int]
-update matrix = [left ++ [left' ++ [val] ++ right'] ++ right | val <- [1..9]]
+update matrix =
+  [left ++ [left' ++ [val] ++ right'] ++ right | val <- [1..9]]
   where
     (left, fit:right) = break (any (== 0)) matrix
-    (left', _:right') = break (== 0) fit     
+    (left', _:right') = break (== 0) fit
     
-main :: IO ()
-main = do
-  sudoku <- forM [1..9] (\_ -> map read . words <$> getLine) 
-  let answers = solve sudoku
-  mapM_ (mapM_ (putStrLn . concat . intersperse " " . map show)) answers
+divide :: Int -> [a] -> [[a]]
+divide _ [] = []
+divide n xs = left : divide n right    
+  where
+    (left, right) = splitAt n xs
+    
+duplicate :: Eq a => [a] -> Bool
+duplicate [] = False
+duplicate (x:xs) = elem x xs || duplicate xs     
+    
